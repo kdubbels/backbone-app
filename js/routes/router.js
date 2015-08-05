0d2a22cd4c2becea0550bee1194b87dffbccd736
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2015-08-05 17:03:17 -0400

    hover event added

---
+++ js/routes/router.js
@@ -0,0 +1,15 @@
+// Namespace our flowerApp
+var app = app || {};
+
+app.Router = Backbone.Router.extend({
+
+routes :{
+  "": "noCopy",
+  "heirloomRose" : "heirloomRoseMessage",
+  "rainbowRose": "rainbowRoseMessage",
+  "redRose" : "redRoseMessage"
+},
+
+
+
+});
--- js/views/singleFlowerView.js
+++ js/views/singleFlowerView.js
@@ -13,6 +13,19 @@ app.singleFlowerView = Backbone.View.extend({
     var flowerTemplate = this.template(this.model.toJSON());
     this.$el.html(flowerTemplate);
     return this;
+  },
+
+  events: {
+    'mouseover': 'addBgColor',
+    'mouseout': 'removeBgColor'
+  },
+
+  addBgColor: function() {
+    this.$el.addClass("bgColorImage");
+  },
+
+  removeBgColor: function() {
+    this.$el.removeClass("bgColorImage");
   }
-  
+
 });
